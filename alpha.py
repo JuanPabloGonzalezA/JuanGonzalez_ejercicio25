5d4d1437d66ff8ebbb3b1a179f58279582a06250
Nicolas Felipe Vergara Duran <nicolasfelipevergaraduran@ELECTRIC.uniandes.edu.co>
2018-05-08 08:35:38 -0500

    creo que hay problemas con la conversion de los datos

--- alpha.py
+++ alpha.py
@@ -4,22 +4,23 @@ import matplotlib.pyplot as plt
 G=6.67408e-11
 M=1.989e30
 
-rm=np.sqrt(2.133679072387218E-01**2+3.727310311768299E-01**2+5.058864779204983E-02**2)*1.496e+11
-vm=np.sqrt(1.892057222101034E-02**2+1.513838055059527E-02**2 +4.995475648824679E-04**2)*1.7315e6
+rm=np.sqrt(2.133679072387218E-01**2+3.727310311768299E-01**2+5.058864779204983E-02**2)*1.496E+11
+vm=np.sqrt(1.892057222101034E-02**2+1.513838055059527E-02**2 +4.995475648824679E-04**2)*1.7315E+6
 
-alpha=np.linspace(0,4,100)
+alpha=np.linspace(0,3,300)
 
 beta=vm**2/(rm**(1-alpha))
-print beta
 
 betabarra=G*M
 
-proba=np.exp(-1.0*(betabarra-beta)**2/(2*np.var(beta)))
-print proba
+proba=np.exp(-((betabarra-beta)**2/(2*np.var(beta))))
 
 probaalpha=1/4
 
 probafinal=probaalpha*proba
-
+#plt.plot(alpha,beta)
 plt.plot(beta,proba)
+inmax=np.argmax(proba)
+plt.scatter(beta[inmax],proba[inmax],label=str(alpha[inmax]))
+plt.legend()
 plt.show()
